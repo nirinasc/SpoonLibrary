520ec953334a12d4e6d6da94b4087539d3b49bd9
nirinasc <nirina.razafimahafaly@santoshaconsulting.com>
2019-02-27 17:39:05 +0300

    set logs managment

---
+++ app/admin/logs.rb
@@ -0,0 +1,66 @@
+ActiveAdmin.register Log do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+    scope 'Books Loan', :book_loan, default: true
+    scope 'Books Return', :book_return
+
+    filter :user 
+    filter :book
+    filter :date
+    filter :returned
+
+    index do
+        id_column
+        column :user 
+        column :book
+        column 'Referenced Book Loan' do |book_return|
+            link_to book_return.loan
+        end if params[:scope] == 'books_return'
+        column :date
+        column :due_date if params[:scope] == 'books_loan'
+        column :returned if params[:scope] == 'books_loan'
+        column 'Recorded at', :created_at
+        actions
+    end
+
+    #show page
+    show do
+        attributes_table do
+            row :user
+            row :book
+            row :date
+            row "Referenced Book Loan" do |book_return|
+                link_to book_return.loan
+            end if resource.book_return?
+            row :returned if resource.book_loan?
+            row "Recored at", :created_at
+        end
+    end
+
+    #form
+    permit_params :user_id, :book_id, :loan_id, :classification, :date, :due_date, :returned
+
+    form do |f|
+        inputs do
+            f.input :user, input_html: { disabled: f.object.persisted? ? true : false }
+            f.input :book, input_html: { disabled: f.object.persisted? ? true : false }
+            f.input :loan, as: 'select', collection: Log.book_loan.collect{ |book_loan| [book_loan, book_loan.id] }, allow_blank: true, input_html: { disabled: f.object.persisted? ? true : false }
+            f.input :classification, label: 'Type' unless f.object.persisted?
+            f.input :date
+            f.input :due_date unless f.object.persisted? && f.object.book_return?
+            f.input :returned unless f.object.persisted? && f.object.book_return?
+        end
+        actions
+    end
+
+end
--- app/models/log.rb
+++ app/models/log.rb
@@ -4,11 +4,11 @@ class Log < ApplicationRecord
 
   belongs_to :user
   belongs_to :book
-  has_one :return, class_name: 'Log', foreign_key: 'loan_id'
+  has_one :return, class_name: 'Log', foreign_key: 'loan_id', dependent: :destroy
   belongs_to :loan, class_name: 'Log', optional: true
 
   validates :classification, inclusion: { in: classifications.keys }
-  validates :loan, presence: { if: -> { self.book_return? } }, absence: { if: -> { self.book_loan? }}
+  validates :loan, presence: { if: -> { self.book_return? } }, absence: { if: -> { self.book_loan? }}, uniqueness: true 
   validates_presence_of :date
   validates_datetime :date, if: -> { self.book_loan? }
   validates_datetime :date, after: :associated_loan_date, if: -> { self.book_return? }
@@ -16,6 +16,19 @@ class Log < ApplicationRecord
   validates_datetime :due_date, after: :date, if: -> { self.book_loan? }
   validates :returned, inclusion: { in: [true, false] }
 
+  before_save :set_owners, if: -> { self.book_return? } 
+
+
+  def set_owners 
+    self.user = self.loan.user
+    self.book = self.loan.book
+  end
+
+  def to_s
+    self.book_loan? ? "#{self.book.name} Book Loan ##{self.date}" : "#{self.book.name} Book Return ##{self.date}"
+  end
+
+  private
   def associated_loan_date
     self.loan.date
   end
