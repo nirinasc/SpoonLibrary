cfae9419e3c2449b822fb85d557471497b51e3f2
nirinasc <nirina.razafimahafaly@santoshaconsulting.com>
2019-03-05 15:00:12 +0300

    set admin dashboard page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
 
   enum role: { member: 0, admin: 1 }
 
+  scope :inactive, -> { where(active: false)}
+
   has_many :comments
   has_many :logs
   has_many :books, through: :logs
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :authentication_keys => [:username]
